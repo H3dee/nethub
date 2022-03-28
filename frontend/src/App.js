e621a8745cfd2ab64d1aa05f4701411fd2eb15c1
H3dee <danya.grushkin@gmail.com>
2022-03-28 23:04:38 +0300

    feature1-continious-authentication: project structure prettified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Registration from './pages/Registration';
-import Home from './pages/Home'
-import Authorization from "./pages/Authorization";
+import Home from './pages/Home';
+import Authorization from './pages/Authorization';
 import Topology from './pages/Topology';
 import Main from './pages/Main';
 import ClientTopology from './pages/ClientTopology';
@@ -10,20 +10,20 @@ import Admin from './pages/Admin';
 import NotFound from './pages/NotFound';
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/registration' component={Registration} />
-                <Route path='/' exact component={Home} />
-                <Route path='/authorization' component={Authorization}/>
-                <Route path='/topology' component={Topology}/>
-                <Route path='/main' exact component={Main}/>
-                <Route path='/client_topology' component={ClientTopology}/>
-                <Route path='/admin' component={Admin}/>
-                <Route component={NotFound}/>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route path="/registration" component={Registration} />
+        <Route path="/" exact component={Home} />
+        <Route path="/authorization" component={Authorization} />
+        <Route path="/topology" component={Topology} />
+        <Route path="/main" exact component={Main} />
+        <Route path="/client_topology" component={ClientTopology} />
+        <Route path="/admin" component={Admin} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
