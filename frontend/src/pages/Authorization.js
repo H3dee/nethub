75b4158bbbcc315a6b5d6506aa33520234f01338
H3dee <danya.grushkin@gmail.com>
2022-04-14 00:38:49 +0300

    feature1-continious-authentication: collecting of raw data added

--- frontend/src/pages/Authorization.js
+++ frontend/src/pages/Authorization.js
@@ -1,85 +1,192 @@
-import React, { Component } from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { Redirect, Link, useLocation } from 'react-router-dom';
 
 import { api } from '../services/API';
 
-class Authorization extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
+const eventIds = {
+  MOUSE_MOVE: 512,
+  MOUSE_LEFT_DOWN: 513,
+  MOUSE_LEFT_UP: 514,
+};
 
-      error_password: '',
-      error_email: '',
-      redirect: null,
+const eventNames = {
+  [eventIds.MOUSE_MOVE]: 'mouse move',
+  [eventIds.MOUSE_LEFT_DOWN]: 'mouse left down',
+  [eventIds.MOUSE_LEFT_UP]: 'mouse up down',
+};
+
+export const Authorization = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  });
+  const [redirect, setRedirect] = useState(null);
+  const [isPageObservable, setIsPageObservable] = useState(false);
+
+  const location = useLocation();
+
+  const dataChunksRef = useRef([[]]);
+
+  const timeoutRef = useRef(null);
+  const intervalRef = useRef(null);
+
+  const createEventHandler = (type) => (event) => {
+    event.persist();
+
+    if (!isPageObservable) {
+      return;
+    }
+
+    console.log(event);
+
+    const eventId = eventIds[type];
+    const timestamp = +new Date();
+
+    const chunk = {
+      eventId,
+      event: eventNames[eventId],
+      windowName: location.pathname,
+      timestamp,
+      positionX: event.pageX,
+      positionY: event.pageY,
     };
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onChangeLogin = this.onChangeLogin.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-  }
 
-  onSubmit(event) {
+    const containerLength = dataChunksRef.current.length;
+    const isDataCollectingInitial = containerLength === 1;
+    dataChunksRef.current[isDataCollectingInitial ? 0 : containerLength - 1].push(chunk);
+  };
+
+  const onChange = (event) => {
+    event.persist();
+
+    setForm((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
+  const onMouseMove = createEventHandler('MOUSE_MOVE');
+
+  const onMouseLeftDown = createEventHandler('MOUSE_LEFT_DOWN');
+
+  const onMouseLeftUp = createEventHandler('MOUSE_LEFT_UP');
+
+  const onStartObserving = () => {
+    setIsPageObservable(true);
+
+    timeoutRef.current = setTimeout(() => {
+      const intervalHandler = () => dataChunksRef.current.push([]);
+
+      intervalRef.current = setInterval(intervalHandler, 5000);
+      timeoutRef.current = null;
+    }, 20_000);
+  };
+
+  const onStopObserving = () => {
+    const plannedTimeout = timeoutRef.current;
+    const isInTimeout = plannedTimeout !== null;
+
+    if (isInTimeout) {
+      clearTimeout(plannedTimeout);
+    }
+
+    clearInterval(intervalRef.current);
+    setIsPageObservable(false);
+
+    console.log(dataChunksRef.current);
+
+    dataChunksRef.current = [[]];
+  };
+
+  const onSubmit = (event) => {
     event.preventDefault();
 
-    let a = api.MakeAuthRequest(this.state.email, this.state.password);
-    a.then((res) => {
-      if (!res) return;
-      if (res.data['status'] === 'OK') {
-        this.setState({ redirect: res.data['role'] });
-      }
-    }).catch(async (e) => {
-      if (e.response.data['status'] === '401')
-        await this.setState({ error_password: e.response.data['error'] });
-      else this.setState({ error_password: '' });
-      if (e.response.data['status'] === '418')
-        await this.setState({ error_email: e.response.data['error'] });
-      else this.setState({ error_email: '' });
-    });
+    api
+      .MakeAuthRequest(form.email, form.password)
+      .then((res) => {
+        if (!res) {
+          return;
+        }
+
+        if (res.data['status'] === 'OK') {
+          setRedirect(res.data['role']);
+        }
+      })
+      .catch(async (e) => {
+        if (e.response.data['status'] === '401') {
+          setErrors((prev) => ({ ...prev, password: e.response.data['error'] }));
+        } else {
+          setErrors((prev) => ({ ...prev, password: '' }));
+        }
+
+        if (e.response.data['status'] === '418') {
+          setErrors((prev) => ({ ...prev, email: e.response.data['error'] }));
+        } else {
+          setErrors((prev) => ({ ...prev, email: '' }));
+        }
+      });
+  };
+
+  if (redirect === 'admin') {
+    return <Redirect to="/topology" />;
+  } else if (redirect === '') {
+    return <Redirect to="/main" />;
   }
 
-  onChangeLogin(event) {
-    this.setState({ email: event.target.value });
-  }
-
-  onChangePassword(event) {
-    this.setState({ password: event.target.value });
-  }
-
-  render() {
-    if (this.state.redirect === 'admin') return <Redirect to="/topology" />;
-    else if (this.state.redirect === '') return <Redirect to="/main" />;
-    return (
-      <div className="full-screen background centering">
-        <form className="window" onSubmit={this.onSubmit}>
-          <div className="title title-centering margin-bottom-20">Welcome!</div>
-          <div className="input margin-bottom-20">
-            <div className={`input-header ${this.state.error_email && 'error'} margin-bottom-8`}>
-              e-mail
-              {this.state.error_email && <span className="mini-msg">{this.state.error_email}</span>}
-            </div>
-            <input className="input-field" onChange={this.onChangeLogin} />
+  return (
+    <div
+      onMouseUp={onMouseLeftUp}
+      onMouseMove={onMouseMove}
+      onMouseDown={onMouseLeftDown}
+      className="full-screen background centering"
+    >
+      <button
+        onClick={onStartObserving}
+        disabled={isPageObservable}
+        style={{ position: 'absolute', top: '15px', left: '15px' }}
+      >
+        START OBSERVING
+      </button>
+      <button
+        onClick={onStopObserving}
+        disabled={!isPageObservable}
+        style={{ position: 'absolute', top: '55px', left: '15px' }}
+      >
+        STOP OBSERVING
+      </button>
+      <form className="window" onSubmit={onSubmit}>
+        <div className="title title-centering margin-bottom-20">Welcome!</div>
+        <div className="input margin-bottom-20">
+          <div className={`input-header ${errors.email && 'error'} margin-bottom-8`}>
+            e-mail
+            {errors.email && <span className="mini-msg">{errors.email}</span>}
           </div>
-          <div className="input margin-bottom-40">
-            <div className={`input-header ${this.state.error_password && 'error'} margin-bottom-8`}>
-              password
-              {this.state.error_password && (
-                <span className="mini-msg">{this.state.error_password}</span>
-              )}
-            </div>
-            <input type="password" className="input-field" onChange={this.onChangePassword} />
+          <input value={form.email} name="email" className="input-field" onChange={onChange} />
+        </div>
+        <div className="input margin-bottom-40">
+          <div className={`input-header ${errors.password && 'error'} margin-bottom-8`}>
+            password
+            {errors.password && <span className="mini-msg">{errors.password}</span>}
           </div>
-          <button className="button button-base margin-bottom-8">Sign in</button>
-          <div className="mini-text">
-            Need account?{' '}
-            <Link to="/registration" className="link">
-              Sign up
-            </Link>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+          <input
+            type="password"
+            name="password"
+            onChange={onChange}
+            value={form.password}
+            className="input-field"
+          />
+        </div>
+        <button className="button button-base margin-bottom-8">Sign in</button>
+        <div className="mini-text">
+          Need account?{' '}
+          <Link to="/registration" className="link">
+            Sign up
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Authorization;
