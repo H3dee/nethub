98865727b4ead2bfb391b7ffff8e873de8c028ca
H3dee <danya.grushkin@gmail.com>
2022-05-05 22:41:17 +0300

    feature1-continious-authentication: constants added

---
+++ frontend/src/util/continiousAuthConstants.js
@@ -0,0 +1,15 @@
+export const MS_PER_SECOND = 1_000;
+export const REGULAR_INTERVAL_TIME = 5_000;
+export const INITIAL_INTERVAL_TIME = 20_000;
+
+export const eventIds = {
+  MOUSE_MOVE: 512,
+  MOUSE_LEFT_DOWN: 513,
+  MOUSE_LEFT_UP: 514,
+};
+
+export const eventNames = {
+  [eventIds.MOUSE_MOVE]: 'mouse move',
+  [eventIds.MOUSE_LEFT_DOWN]: 'mouse left down',
+  [eventIds.MOUSE_LEFT_UP]: 'mouse left up',
+};
